c3d44325ab597364e8bb2ccaba6317e1651d00c0
anudeepr <anudeep.reddy1725@gmail.com>
2024-05-22 21:49:19 -0700

    position road on surface

--- road.js
+++ road.js
@@ -2,10 +2,12 @@ import * as THREE from 'three';
 
 class Road {
     constructor(length) {
-        this.geometry = new THREE.BoxGeometry(length, 30, 0.1);
+        this.thickness = 0.1;
+        this.geometry = new THREE.BoxGeometry(length, 30, this.thickness);
         this.material = new THREE.MeshPhongMaterial({color: "black"});
 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.position.set(0, 0, -this.thickness);
     }
 }
 
